b7a0e83c9e42440c3a64ccfc92baa4e183ca52b2
Mudit Ameta <zeusdeux@gmail.com>
2015-05-14 03:05:29 -0400

    Undo the unnecessary component I created. Gah. Apologies.

--- index.jsx
+++ index.jsx
@@ -1,14 +1,7 @@
 var React = require('react');
 var SearchContainer = require('./SearchContainer');
 
-var MainClass = React.createClass({
-  render: function () {
-    'use strict';
-    return (
-      <SearchContainer />,
-      document.getElementById('content')
-    );
-  }
-});
-
-module.exports = MainClass;
+React.render(
+  <SearchContainer />,
+  document.getElementById('content')
+);
